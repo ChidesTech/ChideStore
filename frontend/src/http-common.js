381489f3b73e101b22d69340d84a19798fc86384
MAC <user@Chides-Technologies.local>
2023-06-20 14:17:26 +0100

    new-prods

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const http = axios.create({
-    // baseURL : "https://chidestore-server.onrender.com",
-    baseURL : "http://localhost:5000",
+    baseURL : "https://chidestore-server.onrender.com",
+    // baseURL : "http://localhost:5000",
     headers : {
         "Content-type" : "application/json"
     }
